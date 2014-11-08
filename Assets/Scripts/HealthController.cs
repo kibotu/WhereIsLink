15ca270254b526eb04b5d3a29f6a44621d91c5fa
Sprotte <sprottemail@googlemail.com>
2014-11-09 00:07:49 +0100

    more more more

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthController : MonoBehaviour {
+
+	public int health = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health == 0) {
+			death();
+		}
+	
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "enemy")
+				health--;
+	}
+	void death(){
+		Destroy (this);
+	}
+}
