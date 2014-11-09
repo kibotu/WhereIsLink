c3cf149f8be853d9e21659ee8cac20e571c7b224
Sprotte <s45188@beuth-hochschule.de>
2014-11-09 08:06:23 +0100

    more features

---
+++ Assets/Scripts/Diamonds.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Diamonds : MonoBehaviour
+{
+    public int worth;    
+    public Animator animator;
+
+    
+    void Start()
+    {
+        switch (worth)
+        {
+            case 1:
+                animator.Play("GreenDiamond");
+                break;
+            case 3:
+                animator.Play("BlueDiamond");
+                break;
+            case 5:
+                animator.Play("RedDiamond");
+                break;
+
+            default:
+                animator.Play("GreenDiamond");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+}
+
